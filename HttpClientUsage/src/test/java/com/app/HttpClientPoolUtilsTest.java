08201656e548cb830ef85334eea953f91fca049c
bit1129 <bit1129@gmail.com>
2015-07-08 17:53:54 +0800

    Use log4j  to log the response time
    Use log4j  to log the response time

--- HttpClientUsage/src/test/java/com/app/HttpClientPoolUtilsTest.java
+++ HttpClientUsage/src/test/java/com/app/HttpClientPoolUtilsTest.java
@@ -1,16 +1,12 @@
 package com.app;
 
-import com.app.HttpUtils;
 import org.junit.Test;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpClientPoolUtilsTest {
 
-    private static final String LOG_DIR_NAME = "c:/http.client.logs";
-    private static final String LOG_FILE_NAME = "http.client.log";
+    private final Logger LOG = LoggerFactory.getLogger("hc4");
 
     //time of the requests
     private int loop = 1000;
@@ -24,33 +20,14 @@ public class HttpClientPoolUtilsTest {
     public void testHttpClientPool() throws Exception {
 
         int i = 0;
-        File logDir = new File(LOG_DIR_NAME);
-        if (!logDir.exists()) {
-            logDir.mkdir();
-        }
-        File logFile = new File(logDir, LOG_FILE_NAME);
-        if (!logFile.exists()) {
-            logFile.createNewFile();
-        } else {
-            logFile.renameTo(new File(logDir, LOG_FILE_NAME + "." + System.currentTimeMillis()));
-            logFile = new File(logDir, LOG_FILE_NAME);
-            logFile.createNewFile();
-        }
         long allStart = System.currentTimeMillis();
         while (i++ < loop) {
             long start = System.currentTimeMillis();
             HttpUtils.httpInvoke(TEST_URL, null, null);
             long a = System.currentTimeMillis() - start;
-            String timeSpent = "" + (a >= 1000 ? a + "(>1000)" : a);
-            writeToFile(timeSpent + "\n", logFile);
+            LOG.info("timeSpent: {}", (a >= 1000 ? a + "(>1000)" : a));
             Thread.sleep(loopInterval);
         }
-        writeToFile("Total time spent: " + (System.currentTimeMillis() - allStart) + "\n", logFile);
-    }
-
-    private void writeToFile(String timeSpent, File logFile) throws IOException {
-        FileWriter f = new FileWriter(logFile, true);
-        f.write(timeSpent);
-        f.close();
+        LOG.info("Total time spent: " + (System.currentTimeMillis() - allStart));
     }
 }
--- HttpClientUsage/src/test/java/com/app/HttpClientUtilTest.java
+++ HttpClientUsage/src/test/java/com/app/HttpClientUtilTest.java
@@ -1,55 +1,29 @@
 package com.app;
 
-import com.app.HttpClientUtil;
-import org.junit.Test;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HttpClientUtilTest {
-    private static final String LOG_DIR_NAME = "c:/http.client.logs";
-    private static final String LOG_FILE_NAME = "http.client.log.2";
-
-
     private int loop = 1000;
+    private int loopInterval = 20;
 
-
-    private int loopInterval = 50;
+    private final Logger LOG = LoggerFactory.getLogger("hc4");
 
     private String TEST_URL = "http://pss.qiyi.domain/services/subscribe/list.htm?authcookie=f8crKciDhLBlYm2ZXIhiAdY6g4vJeHPO1Zm3TxKOGJyXj54m4b1&subscribeType=1&page=1&rows=5&sort=latestUpdate&fields=userinfo,verify_info,space,qiyi_vip_info,private_info";
+
     @Test
     public void testHttpClient() throws Exception {
         int i = 0;
-        File logDir = new File(LOG_DIR_NAME);
-        if (!logDir.exists()) {
-            logDir.mkdir();
-        }
-        File logFile = new File(logDir, LOG_FILE_NAME);
-        if (!logFile.exists()) {
-            logFile.createNewFile();
-        } else {
-            logFile.renameTo(new File(logDir, LOG_FILE_NAME + "." + System.currentTimeMillis()));
-            logFile = new File(logDir, LOG_FILE_NAME);
-            logFile.createNewFile();
-        }
-
         long allStart = System.currentTimeMillis();
         while (i++ < loop) {
             long start = System.currentTimeMillis();
             HttpClientUtil.getInstance().get(TEST_URL);
             long a = System.currentTimeMillis() - start;
-            String timeSpent = "" + (a >= 1000 ? a + "(>1000)" : a);
-            writeToFile(timeSpent + "\n", logFile);
+            LOG.info("timeSpent: {}", (a >= 1000 ? a + "(>1000)" : a));
             Thread.sleep(loopInterval);
         }
-        writeToFile("Total time spent: " + (System.currentTimeMillis() - allStart) + "\n", logFile);
-
-    }
-
-    private void writeToFile(String timeSpent, File logFile) throws IOException {
-        FileWriter f = new FileWriter(logFile, true);
-        f.write(timeSpent);
-        f.close();
+        LOG.info("Total time spent: " + (System.currentTimeMillis() - allStart));
     }
 }
